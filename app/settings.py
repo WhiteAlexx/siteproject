a5c661085a8bff46dd07cdce907b9cf959c3d556
WhiteAlexx <126002944+WhiteAlexx@users.noreply.github.com>
2024-08-23 15:29:22 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = '/opt/build/static'
 
 # STATICFILES_DIRS = [
