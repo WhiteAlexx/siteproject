17e2a4bd2507a36992ff4ba68505605a90a1e970
WhiteAlexx <126002944+WhiteAlexx@users.noreply.github.com>
2024-08-27 08:11:20 +0800

    Update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# add your project directory to the sys.path
+project_home = BASE_DIR
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
 application = get_wsgi_application()
