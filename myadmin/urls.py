f2afcd6f6105d4833d9c23f9f7aed285b27e2128
WhiteAlexx <test.akk.android@gmail.com>
2024-09-01 13:50:43 +0800

    add goods in myadmin

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -8,6 +8,6 @@ app_name = "myadmin"
 urlpatterns = [
     path("search/", views.MyAdminView.as_view(), name="search"),
     path("order_done/", views.orderdone, name="order_done"),
+    path("goods/<slug:category_slug>/", views.MyAdminGoods.as_view(), name="goods"),
     path("<slug:order_status>/", views.MyAdminView.as_view(), name="myadmin"),
-    # path("goods/", views.MyAdminGoods.as_view(), name="goods"),
 ]
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,3 +1,4 @@
+import time
 from django.db.models import Prefetch
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView
@@ -10,7 +11,7 @@ from django.shortcuts import redirect
 from myadmin.mixins import AdminMixin
 from myadmin.utils import q_search
 from common.mixins import CacheMixin, get_context_categories
-from goods.models import Categories
+from goods.models import Categories, Products
 from orders.models import Order, OrderItem
 from users.models import User
 
@@ -89,12 +90,46 @@ def get_orders(status):
             ).order_by("id")
 
 
-class MyAdminGoods(LoginRequiredMixin, TemplateView):
+class MyAdminGoods(LoginRequiredMixin, ListView):
+
+    model = Products
+    # queryset = Products.objects.all().order_by('-id')
     template_name = 'myadmin/goods.html'
+    context_object_name = 'goods'
+    paginate_by = 20
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+
+        query = self.request.GET.get("q")
+
+        if category_slug == "tovary":
+            goods = super().get_queryset().exclude(category__slug__icontains='v-puti').exclude(category__slug__icontains='udalennye')
+        elif category_slug == 'is_neo':
+            # сюда поставить проверку на месяц 2592000сек
+            query_create_date = super().get_queryset().filter(is_neo=True)
+            for product in query_create_date:
+
+                if time.time() - time.mktime(time.strptime(product.created_time_stamp.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")) > 2592000:
+                    product.is_neo = False
+                    product.save()
+            goods = super().get_queryset().filter(is_neo=True)
+        elif category_slug == 'v-puti':
+            goods = super().get_queryset().filter(category__slug__icontains='v-puti')
+        elif category_slug == 'all':
+            goods = super().get_queryset().all()
+        elif query:
+            goods = q_search(query)
+        else:
+            goods = super().get_queryset().filter(category__slug=category_slug)
+
+        return goods
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = get_context_categories()
         context['title'] = 'Товары'
+        context['slug_url'] = self.kwargs.get('category_slug')
+        context['categories'] = get_context_categories()
 
         return context
+
